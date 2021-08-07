110c3b690017ff2487c1863f9ec04daebfb72620
Tristan Sladek <tristansladek@gmail.com>
2021-08-07 12:22:04 -0500

    Initialized Gamemode

---
+++ code/Camera/MMOCamera.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Sandbox
+{
+	public class MMOCamera : Camera
+	{
+		[ConVar.Replicated]
+		public static float abx_zoom_speed { get; set; } = 20.0f;
+
+		private Angles orbitAngles;
+		private float orbitDistance = 150;
+		private float orbitDistanceTarget = 150;
+		private float orbitYawTarget;
+		private bool smoothYawReturn = false;
+		private bool mouse_left = false;
+		private bool mouse_right = false;
+
+		public override void Update()
+		{
+			var pawn = Local.Pawn as AnimEntity;
+			if ( pawn == null ) return;
+			Pos = pawn.Position + Vector3.Up * 64;
+
+			//look follow mouse vs return to back
+			if( mouse_left || mouse_right )
+				Rot = Rotation.From( orbitAngles );
+			
+			if( smoothYawReturn )
+			{
+				orbitAngles.yaw = Rotation.Slerp( Rotation.FromYaw( orbitAngles.yaw ), Rotation.FromYaw( orbitYawTarget ), Time.Delta * 4.0f ).Yaw();
+				Rot = Rotation.From( orbitAngles );
+			}
+
+			//Mouse Scrolling
+			orbitDistanceTarget = orbitDistanceTarget.LerpTo( orbitDistance, Time.Delta * 5.0f );
+			var targetPos = Pos + Rot.Backward * orbitDistanceTarget;
+			var tr = Trace.Ray( Pos, targetPos )
+				.Ignore( pawn )
+				.Radius( 8 )
+				.Run();
+			Pos = tr.EndPos;
+
+			
+			FieldOfView = 75;
+			Viewer = null;
+		}
+
+		public override void BuildInput( InputBuilder input )
+		{
+			orbitDistance -= input.MouseWheel * abx_zoom_speed;
+			orbitDistance = orbitDistance.Clamp( 0, 1000 );
+
+			mouse_left = input.Down( InputButton.Attack1 );
+			mouse_right = input.Down( InputButton.Attack2 );			
+
+			// Camera Rotation on Mouse L or R 
+			if ( mouse_left || mouse_right )
+			{
+				orbitAngles.yaw += input.AnalogLook.yaw;
+				orbitAngles.pitch += input.AnalogLook.pitch;
+				orbitAngles = orbitAngles.Normal;
+				orbitAngles.pitch = orbitAngles.pitch.Clamp( -89, 89 );
+				smoothYawReturn = false;
+			}
+			else
+			{	
+				//Rotate Camera to original position when moving
+				if ( !input.AnalogMove.IsNearlyZero() )
+				{
+					orbitYawTarget = input.ViewAngles.Normal.yaw;					
+				}
+				smoothYawReturn = !input.AnalogMove.IsNearlyZero();
+			}
+			
+			//Character movement based upon mouse
+			if ( !mouse_right )
+				input.AnalogLook = Angles.Zero;
+			else
+            	input.ViewAngles = orbitAngles;
+
+			base.BuildInput( input );
+		}
+	}
+}
---
+++ code/MinimalPlayer.cs
@@ -0,0 +1,126 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace MinimalExample
+{
+	partial class MinimalPlayer : Player
+	{
+		private bool dressed = false;
+		public override void Respawn()
+		{
+			SetModel( "models/citizen/citizen.vmdl" );
+
+			base.Respawn();
+
+			EnableAllCollisions = true;
+			EnableDrawing = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+
+			Camera = new MMOCamera();
+			Animator = new StandardPlayerAnimator();
+			Controller = new WalkController();
+
+			if(!dressed)
+			{
+				var model = Rand.FromArray( new[]
+				{
+					"models/citizen_clothes/trousers/trousers.jeans.vmdl",
+					"models/citizen_clothes/trousers/trousers.lab.vmdl",
+					"models/citizen_clothes/trousers/trousers.police.vmdl",
+					"models/citizen_clothes/trousers/trousers.smart.vmdl",
+					"models/citizen_clothes/trousers/trousers.smarttan.vmdl",
+					"models/citizen_clothes/trousers/trousers_tracksuitblue.vmdl",
+					"models/citizen_clothes/trousers/trousers_tracksuit.vmdl",
+					"models/citizen_clothes/shoes/shorts.cargo.vmdl",
+				} );
+
+				ModelEntity pants = new ModelEntity();
+				pants.SetModel( model );
+				pants.SetParent( this, true );
+				pants.EnableShadowInFirstPerson = true;
+				pants.EnableHideInFirstPerson = true;
+
+				SetBodyGroup( "Legs", 1 );
+				dressed = true;
+			}
+			
+			/*
+			
+
+			//
+			// Use WalkController for movement (you can make your own PlayerController for 100% control)
+			//
+			Controller = new BasePlayerController();
+
+			//
+			// Use StandardPlayerAnimator  (you can make your own PlayerAnimator for 100% control)
+			//
+			Animator = new StandardPlayerAnimator();
+
+			//
+			// Use ThirdPersonCamera (you can make your own Camera for 100% control)
+			//
+			Camera = new ThirdPersonCamera();
+
+			EnableAllCollisions = true;
+			EnableDrawing = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+			*/
+
+
+		}
+
+		/// <summary>
+		/// Called every tick, clientside and serverside.
+		/// </summary>
+		public override void Simulate( Client cl )
+		{
+			base.Simulate( cl );
+
+			//
+			// If you have active children (like a weapon etc) you should call this to 
+			// simulate those too.
+			//
+			SimulateActiveChild( cl, ActiveChild );
+
+			//
+			// If we're running serverside and Attack1 was just pressed, spawn a ragdoll
+			//
+			
+
+			/*
+			if ( IsServer && Input.Pressed( InputButton.Attack1 ) )
+			{
+				var ragdoll = new ModelEntity();
+				ragdoll.SetModel( "models/citizen/citizen.vmdl" );  
+				ragdoll.Position = EyePos + EyeRot.Forward * 40;
+				ragdoll.Rotation = Rotation.LookAt( Vector3.Random.Normal );
+				ragdoll.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+				ragdoll.PhysicsGroup.Velocity = EyeRot.Forward * 1000;
+			}
+			*/
+		}
+
+		public override void OnKilled()
+		{
+			base.OnKilled();
+
+			//EnableDrawing = false;
+		}
+		[Event.Frame]
+		protected void OnFrame()
+		{
+			var pawn = Local.Pawn;
+			if ( pawn == null ) return;
+
+			RenderAlpha = (((Position + Vector3.Up * 64).Distance( Input.Position ) - 5.0f) / 50.0f).Clamp( 0, 1 );
+			foreach( ModelEntity ent in pawn.Children){
+				ent.RenderAlpha = RenderAlpha;
+			}
+
+		}
+	}
+}
